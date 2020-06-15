c42a0d0c3dc75582edd30bdb52a65153b63fc6bf
Lastv25 <66638012+Lastv25@users.noreply.github.com>
2020-06-15 11:06:41 +0200

    test parsing 1

--- scripts/index.js
+++ scripts/index.js
@@ -58,7 +58,7 @@ function shuffle()
 function getInfosonCard(card_name, reversed)
 {
    const csvData = Papa.parse("lastv25.github.io/tarot_meaning.csv", {linebreak:"\n",
-    delimiter: ",",header:false, download:true}).data
+    delimiter: ",",header:true, download:true}).data
    console.log(csvData);
    console.log(csvData.filter(data => data.Name === "The Tower")[0].Name);
 }
